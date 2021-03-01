6113466044bbd3a7d048577dcdec9845c7c03570
GlennV5 <glennjeanyves.k@live.com>
2021-03-01 08:31:10 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Glossaire</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <h1>GLOSSAIRE</h1>
+    <p>Le vocabulaire de l'internet peut parfois paraître obscur, flou,
+voire complètement inconnu pour certains !
+Nous vous proposons donc de découvrir ci-dessous nos propres définitions
+de ce langage possiblement technique ou parfois plus marketing.
+N'hésitez pas à nous contacter si vous avez des questions ou des remarques par rapport à cela !</p>
+    <div class="letter">A</div>
+    <ul>
+                 
+                                <li>
+						 <?php echo '<a href="page1.php"> Agence conseil en communication digitale</a> '?>
+                  </li>
+                                <li>
+                  <a href="">
+                    Agence community management
+                  </a>
+                </li>
+                                <li>
+                  <a href="">
+                    Advergame
+                  </a>
+                </li>
+                                <li>
+                  <a href="">
+                    Ad exchanges
+                  </a>
+                </li>
+                                <li>
+                  <a href="">
+                    Activation digitale
+                  </a>
+                </li>
+                                <li>
+                  <a href="">
+                    Achat média Facebook
+                  </a>
+                </li>
+                                <li>
+                  <a href="">
+                    Acculturation numérique et digitale
+                  </a>
+                </li>
+                                <li>
+                  <a href="">
+                    Account based marketing 
+                  </a>
+                </li>
+                              </ul>
+              
+                <?php include 'footer.php';?>
+    <script src="" async defer></script>
+</body>
+
+
+
+</html>
+
